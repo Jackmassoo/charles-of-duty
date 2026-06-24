afa3107c2f998576db59b7ff8e82b2c7e5ae1868
Jackmassoo <jacques.massoni78@gmail.com>
2026-06-24 11:23:20 +0200

    Initialize Supabase client with URL and key

--- supabase.js
+++ supabase.js
@@ -1,1 +1,11 @@
-// connexion supabase
+const SUPABASE_URL =
+'https://ijdwmltizakkviqlkxvg.supabase.co/rest/v1/';
+
+const SUPABASE_KEY =
+'sb_publishable_iF1Euu7tuQahJvM5ZpN85w_R-vPSM_D';
+
+const supabaseClient =
+supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
